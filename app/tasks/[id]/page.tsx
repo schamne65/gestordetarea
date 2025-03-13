2f151b8095dd7d9f8ae076f825c55572fe60f7d6
Nicolas Schamne <schamne65@gmail.com>
2025-03-13 08:54:09 -0300

    cambios varios

--- app/tasks/[id]/page.tsx
+++ app/tasks/[id]/page.tsx
@@ -113,7 +113,7 @@ export default function TaskDetailPage ({params} : { params : {id : string}}){
         }
     
         fetchTask()
-      }, [params.id, router])
+      }, [task, router])
 
       const formatDate = (dateString : string) => {
         const options : Intl.DateTimeFormatOptions = {year: "numeric" , month : "long" , day : "numeric"}
